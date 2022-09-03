acbf53af6dee3c41b4fe07d1c403b7018da9d118
Jessica <jessgarcez@hotmail.com>
2022-09-03 04:47:10 -0500

    Melhora da funcionalidade do navbar e adicao de separacao visual de sessoes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,22 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
+const navbar = document.querySelector(".topnav");
+const menuItems = document.querySelector(".menu-items");
+const menuItem = document.querySelectorAll(".menu-item");
+
+// main toggle
+navbar.addEventListener("click", () => {
+  toggle();
+});
+
+// toggle on item click if open
+menuItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (navbar.classList.contains("open")) {
+      toggle();
     }
-  }
-  
+  });
+});
+
+function toggle() {
+  navbar.classList.toggle("open");
+  menuItems.classList.toggle("open");
+}
